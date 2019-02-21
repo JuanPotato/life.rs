cb55e18459e90ff5ce6262a8d284219038201cc3
expectocode <expectocode@gmail.com>
2019-02-21 16:18:49 +0000

    structoptify

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,50 @@
 extern crate grid;
+extern crate structopt;
 
 use grid::Grid;
 use std::{thread, time};
+use structopt::StructOpt;
 
-/// The number of columns that will be printed to the terminal (same as the number of columns in
-/// the game grid).
-const WIDTH: usize = 80;
+#[derive(StructOpt, Debug)]
+#[structopt(name = "life-rs")]
+struct Opt {
+    #[structopt(
+        short = "c",
+        long = "columns",
+        default_value = "80",
+        help = "The number of columns that will be printed to the terminal (same as the number of columns in the game grid)"
+    )]
+    columns: usize,
 
-/// The number of rows that will be printed to the terminal.
-///
-/// Note that the number of rows in the game grid is twice this amount, as each printed row is two
-/// game rows.
-const HEIGHT: usize = 20;
+    #[structopt(
+        short = "r",
+        long = "rows",
+        default_value = "20",
+        help = "The number of rows that will be printed to the terminal. Note that the number of rows in the game grid is twice this amount, as each printed row is two game rows."
+    )]
+    rows: usize,
 
-const ALIVE_CHANCE: f64 = 0.2;
-const SLEEP_TIME_MS: u64 = 60;
+    #[structopt(
+        long = "iter-count",
+        help = "BENCHMARKING TOOL: num iterations of game"
+    )]
+    iter_count: Option<usize>,
 
-fn make_grid() -> Grid {
-    let mut grid = Grid::new(WIDTH, HEIGHT * 2);
-    grid.randomise(ALIVE_CHANCE);
-    grid
+    #[structopt(
+        short = "d",
+        long = "delay",
+        default_value = "60",
+        help = "Duration to wait between each generation (milliseconds)"
+    )]
+    delay: u64,
+
+    #[structopt(
+        short = "a",
+        long = "alive-chance",
+        default_value = "0.2",
+        help = "Chance for cells to be alive in the first generation"
+    )]
+    alive_chance: f64,
 }
 
 /// Count the number of neighbouring cells that are alive, not including itself
@@ -57,24 +82,25 @@ fn draw(grid: &Grid) {
 }
 
 fn main() {
-    let mut grid = make_grid();
-    let mut next_grid = Grid::new(WIDTH, HEIGHT * 2);
-    let mut iter_count = usize::max_value();
+    let opt = Opt::from_args();
 
-    if let Some(iters) = std::env::args().nth(1) {
-        iter_count = iters.parse().unwrap_or_else(|e| {
-            panic!(
-                "Could not parse generation count \"{}\" as integer: {}",
-                iters, e
-            )
-        });
-    }
+    let (width, height) = (opt.columns, opt.rows);
+
+    let mut grid = Grid::new(width, height * 2);
+    let mut next_grid = Grid::new(width, height * 2);
+    grid.randomise(opt.alive_chance);
 
     println!("\x1b[2J\x1b[H");
+
+    let iter_count = match opt.iter_count {
+        Some(a) => a,
+        None => usize::max_value(),
+    };
+
     for _ in 0..iter_count {
         draw(&grid);
         generation(&grid, &mut next_grid);
         std::mem::swap(&mut next_grid, &mut grid);
-        thread::sleep(time::Duration::from_millis(SLEEP_TIME_MS));
+        thread::sleep(time::Duration::from_millis(opt.delay));
     }
 }
